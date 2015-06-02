b46123a91777cbad692aed7625049ed9d9b3e3c4
goldenratio1618 <aaatanas@yahoo.com>
2015-06-02 17:58:16 -0500

    Added a boundary box to CLI
    Added a boundary box to the command-line interface, making it easier to
    see the edges of the region (or where things will reappear on the other
    side, if the torus geometry is used)

--- cmdline.py
+++ cmdline.py
@@ -7,19 +7,35 @@ class CmdInterface:
         self.game = game
     
     def run(self, steps, delay):
-        for step in range(steps):
+        """ Runs the Command-Line interface for a specified number of steps,
+            or forever if the number of steps is specified to be -1."""
+        step = 0
+        while step < steps or steps == -1:
             grid_str = "STEP: " + str(step) + "\n"
+            grid_str += CmdInterface.horizontalLine(self.game.dim[1])
             for i in range(self.game.dim[0]):
+                grid_str += "|"
                 for j in range(self.game.dim[1]):
                     if self.game.grid[i][j].val:
                         grid_str += "X"
                     else:
                         grid_str += " "
-                grid_str += "\n"
+                grid_str += "|\n"
             # cross-platform way to clear command prompt, for the next round of
             # printing the grid
+            grid_str += CmdInterface.horizontalLine(self.game.dim[1])
             os.system('cls' if os.name == 'nt' else 'clear')
             print(grid_str)
             sys.stdout.flush()
             self.game.evolve2D()
             sleep(delay)
+            step += 1
+       
+    @staticmethod
+    def horizontalLine(dim):
+        """Draws a horizontal line, with two vertical bars at either end."""
+        line = "|"
+        for _ in range(dim):
+            line += "_"
+        line += "|\n"
+        return line
